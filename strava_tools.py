de26a79f80e41999154f8556d7193ec5ae092d8d
Dave Dexter <dave.dex@gmail.com>
2013-03-15 13:16:39 +0000

    Created a barebones file with a single test in order to test the integration with Jenkins

---
+++ strava_tools.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import unittest
+
+
+class DirectionChecks(unittest.TestCase):
+    def testNWDirection(self):
+        '''Tests the direction returned between two coords lying in a NW direction is correct.'''
+        self.failUnless(False)
+
+
+def main():
+    unittest.main()
+
+if __name__ == "__main__":
+    main()
