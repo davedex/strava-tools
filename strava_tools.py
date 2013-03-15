052583f4cf4dea47bf2aef86a06ad61391169542
Dave Dexter <dave.dex@gmail.com>
2013-03-15 16:22:25 +0000

    Implemented functionality to get a bearing between two points.

--- strava_tools.py
+++ strava_tools.py
@@ -2,12 +2,19 @@
 
 import unittest
 import xmlrunner
+from math import atan2, degrees
 
 
 class DirectionChecks(unittest.TestCase):
     def testNWDirection(self):
         '''Tests the direction returned between two coords lying in a NW direction is correct.'''
-        self.failUnless(False)
+        self.failUnless(getBearing((0,0), (10, 10)) == 45)
+
+
+def getBearing((start_lat, start_lng), (end_lat, end_lng)):
+    '''Returns the initial bearing between two lat lng points.
+    http://www.movable-type.co.uk/scripts/latlong.html'''
+    return(degrees(atan2(end_lng - start_lng, end_lat - start_lat)))
 
 
 def main():
