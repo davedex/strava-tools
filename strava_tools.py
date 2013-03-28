72d17e1238ae45a318c260ad14766c75cdcf7372
Dave Dexter <dave.dex@gmail.com>
2013-03-28 16:40:23 +0000

    Added a function to reverse geocode a location from a lat, lng

--- strava_tools.py
+++ strava_tools.py
@@ -3,8 +3,24 @@
 import unittest
 import xmlrunner
 from math import atan2, degrees
+from geopy import geocoders
 
 class BearingError(Exception): pass
+
+def getBearing((start_lat, start_lng), (end_lat, end_lng)):
+    '''Returns the initial bearing between two lat lng points.
+    http://www.movable-type.co.uk/scripts/latlong.html'''
+    if not ((-90 <= start_lat <= 90) and (-90 <= start_lng <= 90) and
+            (-90 <= end_lat <= 90) and (-90 <= end_lng <= 90)):
+        raise(BearingError)
+    return(degrees(atan2(end_lng - start_lng, end_lat - start_lat)))
+
+def getLocation(lat, lng):
+    '''Uses geopy's reverse geocoding to get a location from a point'''
+    g = geocoders.GeoNames()
+    (place, point) = g.reverse((lat, lng))
+    return place
+
 
 class BearingChecks(unittest.TestCase):
     def testKnownBearing(self):
@@ -21,14 +37,10 @@ class BearingChecks(unittest.TestCase):
         self.failUnlessRaises(BearingError, getBearing, (-180,180), (120,120))
 
 
-def getBearing((start_lat, start_lng), (end_lat, end_lng)):
-    '''Returns the initial bearing between two lat lng points.
-    http://www.movable-type.co.uk/scripts/latlong.html'''
-    if not ((-90 <= start_lat <= 90) and (-90 <= start_lng <= 90) and
-            (-90 <= end_lat <= 90) and (-90 <= end_lng <= 90)):
-        raise(BearingError)
-    return(degrees(atan2(end_lng - start_lng, end_lat - start_lat)))
-
+class ReverseGeocode(unittest.TestCase):
+    def testKnownLocation(self):
+        '''Tests a known location can be looked up from it's coordinates'''
+        self.failUnless(getLocation(37.41785, -122.12793) == u"3998 Ventura Ct, Palo Alto, US 94306")
 
 def main():
     unittest.main(testRunner=xmlrunner.XMLTestRunner(output='test_reports'))
